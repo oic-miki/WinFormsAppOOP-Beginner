d4bb66b9ff31f970c1b12874d1f18ed6de576f8c
miki@oic.jp <miki@oic.jp>
2024-01-10 09:50:07 +0900

    テストプロジェクトの追加

---
+++ WinFormsAppOOPTest-Beginner/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WinFormsAppOOPTest_Beginner
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
