154e3d629d1e5451e4b70632e15e28df9ac07619
miki@oic.jp <miki@oic.jp>
2024-01-10 10:06:58 +0900

    Person作成前

--- WinFormsAppOOPTest-Beginner/PersonTest.cs
+++ WinFormsAppOOPTest-Beginner/PersonTest.cs
@@ -1,8 +1,13 @@
+using System;
+
 namespace WinFormsAppOOPTest_Beginner
 {
     [TestClass]
     public class PersonTest
     {
+
+        private Person person = new Person();
+
         [TestMethod]
         public void getNameTest()
         {
